25233dafbb79c9b7e545d8e161e5b734b610e726
mliberato <liberato.myguel@gmail.com>
2013-11-07 10:49:26 -0500

    configuracion de recursos para login y redirecionamiento ademas de i18n

--- spring-jsf/src/main/java/pe/edu/cibertec/security/domain/Usuario.java
+++ spring-jsf/src/main/java/pe/edu/cibertec/security/domain/Usuario.java
@@ -1,5 +1,38 @@
 package pe.edu.cibertec.security.domain;
 
-public class Usuario {
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long usuarioId;
+	private String usuario;
+	private String password;
+
+	public Long getUsuarioId() {
+		return usuarioId;
+	}
+
+	public void setUsuarioId(Long usuarioId) {
+		this.usuarioId = usuarioId;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
