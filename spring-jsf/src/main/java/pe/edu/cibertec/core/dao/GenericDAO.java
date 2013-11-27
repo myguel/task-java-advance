877741739e7cc25e50e53d311b9bd4801f1fc387
jpadilla <jpadilla@LAP0466.luzdelsur.com.pe>
2013-11-26 23:43:30 -0500

    [Avance de la implementación de Auditoria Parte 3/JuanPadilla/12-11-2013]

--- spring-jsf/src/main/java/pe/edu/cibertec/core/dao/GenericDAO.java
+++ spring-jsf/src/main/java/pe/edu/cibertec/core/dao/GenericDAO.java
@@ -6,9 +6,29 @@ import java.util.List;
 public interface  GenericDAO<T extends Serializable>  {
 
 	 long count();
+         /**
+         * <p>Metodo que permite persistir en db</p>
+	 * @param t
+	 */
      T save(T t);
+     /**
+	 * <p>Metodo que permite eliminar un registro en db.</p>
+	 * @param id
+	 */
      void delete(Object id);
+     /**
+	 * <p>Metodo que permite obtener un registro de la db.</p>
+	 * @param id
+	 */
      T get(Object id);    
+     /**
+	 * <p>Metodo que permite listar los registros de una tabla en db.</p>
+	 * sin parámetros
+	 */
      List<T> getAll();
+     /**
+	 * <p>Metodo que permite actualizar un registro en db.</p>
+	 * @param t
+	 */
      T update(T t);
 }
--- spring-jsf/src/main/java/pe/edu/cibertec/core/service/GenericService.java
+++ spring-jsf/src/main/java/pe/edu/cibertec/core/service/GenericService.java
@@ -19,10 +19,23 @@ public interface GenericService<T> {
 	 */
 	@PreAuthorize("hasAnyRole('ROL_ADMINISTRADOR', 'ROL_DIRECTOR','ROL_USER')")
 	void save(T t);
+        
+        /**
+	 * <p>Metodo que permite actualizar un registro en db, a su vez el metodo es securizado para algunos perfiles.</p>
+	 * @param t
+	 */
 	@PreAuthorize("hasAnyRole('ROL_ADMINISTRADOR', 'ROL_DIRECTOR')")
 	void update(T t);
+        /**
+	 * <p>Metodo que permite eliminar en db, a su vez el metodo es securizado para algunos perfiles.</p>
+	 * @param t
+	 */
 	@PreAuthorize("hasAnyRole('ROL_ADMINISTRADOR')")
 	void delete(T t);
 	T getById(T t);
+        /**
+	 * <p>Metodo que permite listar los registros de una tabla en db, el metodo no es securizado.</p>
+	 * sin parámetros
+	 */
 	List<T>  getAll();
 }
