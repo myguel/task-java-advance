15d5d6f52b98540d35a2f9786c87d22432551501
myguel <liberato.myguel@gmail.com>
2013-11-26 22:13:59 -0500

    modificacion y documentacion

--- spring-jsf/src/main/java/pe/edu/cibertec/core/service/GenericService.java
+++ spring-jsf/src/main/java/pe/edu/cibertec/core/service/GenericService.java
@@ -3,9 +3,20 @@ package pe.edu.cibertec.core.service;
 import java.util.List;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-
+/**
+ * 
+ * @author myguel
+ *
+ *@version 1.0
+ * <p> Interface generica que abstrae los metodos mas comunes en el sistema</p>
+ * @param <T>
+ */
 public interface GenericService<T> {
 
+	/**
+	 * <p>Metodo que permite persistir en db, a su vez el metodo es securizado para algunos perfiles.</p>
+	 * @param t
+	 */
 	@PreAuthorize("hasAnyRole('ROL_ADMINISTRADOR', 'ROL_DIRECTOR','ROL_USER')")
 	void save(T t);
 	@PreAuthorize("hasAnyRole('ROL_ADMINISTRADOR', 'ROL_DIRECTOR')")
--- spring-jsf/src/main/java/pe/edu/cibertec/core/service/impl/LaboratorioServiceImpl.java
+++ spring-jsf/src/main/java/pe/edu/cibertec/core/service/impl/LaboratorioServiceImpl.java
@@ -21,6 +21,9 @@ public class LaboratorioServiceImpl implements LaboratorioService {
 	
 	@Autowired
 	private LaboratorioDAO laboratorioDAO;
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	@Transactional(readOnly=false,propagation=Propagation.REQUIRED,rollbackFor=BusinessException.class)
 	public void save(Laboratorio laboratorio)  throws BusinessException{
